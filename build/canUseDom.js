354530e5953f5928c8b472e6fb72f2694f799518
Jeremy Gayed <tizmagik@yahoo.com>
2017-10-09 02:04:39 -0400

    gitignore

--- build/canUseDom.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var canUseDOM = !!(typeof window !== 'undefined' && window.document);
-
-exports.default = canUseDOM;
