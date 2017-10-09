29772092162dad79a8fd282e166ab38b3e317f52
Jeremy Gayed <tizmagik@yahoo.com>
2017-10-09 00:53:04 -0400

    More cleanup

--- example/src/Home.js
+++ example/src/Home.js
@@ -1,36 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
+import HeadTag from 'react-head';
 import logo from './react.svg';
 import './Home.css';
-import HeadTag from 'react-head';
 
-class Home extends Component {
-  render() {
-    return (
-      <div className="Home">
-        <HeadTag tag="title">Title of page</HeadTag>
-        <HeadTag tag="link" rel="canonical" content="http://jeremygayed.com/" />
-        <HeadTag tag="meta" name="example" content="whatever" />
-        <div className="Home-header">
-          <img src={logo} className="Home-logo" alt="logo" />
-          <h2>react-head example</h2>
-        </div>
-        <p className="Home-intro">
-          View the example code in <code>src/Home.js</code>. Note that this works isomorphically.
-        </p>
-        <ul className="Home-resources">
-          <li>
-            <a href="https://github.com/jaredpalmer/razzle">Docs</a>
-          </li>
-          <li>
-            <a href="https://github.com/jaredpalmer/razzle/issues">Issues</a>
-          </li>
-          <li>
-            <a href="https://palmer.chat">Community Slack</a>
-          </li>
-        </ul>
-      </div>
-    );
-  }
-}
+const Home = () => (
+  <div className="Home">
+    <HeadTag tag="title">Example react-head App</HeadTag>
+    <HeadTag tag="link" rel="canonical" content="http://jeremygayed.com/" />
+    <HeadTag tag="meta" name="example" content="whatever" />
+    <div className="Home-header">
+      <img src={logo} className="Home-logo" alt="logo" />
+      <h2>react-head example</h2>
+    </div>
+    <p className="Home-intro">
+      View the example code in <code>src/Home.js</code>. Note that this works isomorphically.
+    </p>
+    <ul className="Home-resources">
+      <li>
+        <a href="https://github.com/tizmagik/react-head">Docs</a>
+      </li>
+      <li>
+        <a href="https://github.com/tizmagik/react-head/issues">Issues</a>
+      </li>
+    </ul>
+  </div>
+);
 
 export default Home;
--- example/src/server.js
+++ example/src/server.js
@@ -31,7 +31,6 @@ server
     <head>
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta charSet='utf-8' />
-        <title>Welcome to Razzle</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         ${assets.client.css ? `<link rel="stylesheet" href="${assets.client.css}">` : ''}
         ${process.env.NODE_ENV === 'production'
