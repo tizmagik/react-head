3518f6108358425842cca405edd7337d274c3361
Justin Tanacredi <jtanacredi@gmail.com>
2017-10-10 17:19:37 -0400

    Add new contact route example

--- example/src/App.js
+++ example/src/App.js
@@ -7,6 +7,7 @@ import './App.css';
 const App = () => (
   <Switch>
     <Route exact path="/" component={Home} />
+    <Route exact path="/contact" component={Contact} />
   </Switch>
 );
 
---
+++ example/src/Contact.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import HeadTag from 'react-head';
+import { Link } from 'react-router-dom';
+import logo from './react.svg';
+import './Contact.css';
+
+const Contact = () => (
+  <div className="Contact">
+    <HeadTag tag="title">Example Contact Us Page</HeadTag>
+    <HeadTag tag="link" rel="canonical" content="http://jeremygayed.com/" />
+    <HeadTag tag="meta" name="keywords" content="Contact,Example,React,Header" />
+    <div className="Contact-header">
+      <img src={logo} className="Contact-logo" alt="logo" />
+      <h2>react-head contact us page</h2>
+    </div>
+    <p className="Contact-intro">
+      View the example code in <code>src/Contact.js</code>. Note that this works isomorphically.
+    </p>
+    <p>Click the example home page below to see how the Header tags will update</p>
+    <Link to='/'>Home</Link>
+    <ul className="Contact-resources">
+      <li>
+        <a href="https://github.com/tizmagik/react-head">Docs</a>
+      </li>
+      <li>
+        <a href="https://github.com/tizmagik/react-head/issues">Issues</a>
+      </li>
+    </ul>
+  </div>
+);
+
+export default Contact;
