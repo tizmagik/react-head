95829d3781ab1f202d9772dd1d2ff57f8d18e8a7
Joshua Kidd <kidd.josh.343@gmail.com>
2017-10-11 04:04:59 -0500

    Removed brackets

--- example/src/Home.js
+++ example/src/Home.js
@@ -5,12 +5,12 @@ import './Home.css';
 
 const Home = () => (
   <div className="Home">
-    {<Title>This is a react app!</Title>}
+    <Title>This is a react app!</Title>
     {/*<HeadTag tag="title">Example react-head App</HeadTag>*/}
-    {<Link rel="canonical" content="http://jeremygayed.com" />}
+    <Link rel="canonical" content="http://jeremygayed.com" />
     {/*<HeadTag tag="link" rel="canonical" content="http://jeremygayed.com/" />*/}
     {/*<HeadTag tag="meta" name="example" content="whatever" />*/}
-    {<Meta name="example" content="whatever" />}
+    <Meta name="example" content="whatever" />
     <div className="Home-header">
       <img src={logo} className="Home-logo" alt="logo" />
       <h2>react-head example</h2>
