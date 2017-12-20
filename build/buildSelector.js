c17b72d36a3345069a143c21cd9480f498770b48
jamesjjk <james.klymowsky@peddler.com>
2017-12-20 18:33:32 +0100

    Fixed build selector

--- build/buildSelector.js
+++ build/buildSelector.js
@@ -3,9 +3,11 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+/* eslint no-useless-escape:0 */
 var buildSelector = function buildSelector(obj) {
   return Object.keys(obj).map(function (k) {
-    return '[' + k + '="' + obj[k] + '"]';
+    var cnt = obj[k].replace(/["\\]/g, '\\$&');
+    return '[' + k + '="' + cnt + '"]';
   }).join('');
 };
 
