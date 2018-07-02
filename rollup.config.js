2f0badf0cfa96ad1489c89366a250ee7508a41ce
Bogdan Chadkin <trysound@yandex.ru>
2018-07-03 01:08:03 +0300

    Provide umd bundle and prefer builtins in helpers (#34)
    After discussion in https://github.com/tizmagik/react-head/pull/26
    I decided to removed polyfills from babel since they all covers only
    es5. This trimmed final bundle size significantly from 5kb gzipped to
    1kb.
    
    To track this I provided umd bundle in size snapshot.

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,4 @@
+import nodeResolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import { sizeSnapshot } from 'rollup-plugin-size-snapshot';
 import pkg from './package.json';
@@ -9,10 +10,33 @@ const external = id => !id.startsWith('/') && !id.startsWith('.');
 
 const getBabelOptions = ({ useESModules }) => ({
   runtimeHelpers: true,
-  plugins: [['@babel/transform-runtime', { polyfill: false, useESModules }]],
+  plugins: [['@babel/transform-runtime', { useBuiltIns: true, useESModules }]],
 });
 
+const globals = {
+  react: 'React',
+  'react-dom': 'ReactDOM',
+  'prop-types': 'PropTypes',
+};
+
 export default [
+  {
+    input,
+    output: {
+      file: 'dist/index.umd.js',
+      format: 'umd',
+      exports: 'named',
+      name: 'ReactHead',
+      globals,
+    },
+    external: Object.keys(globals),
+    plugins: [
+      nodeResolve(),
+      babel(getBabelOptions({ useESModules: true })),
+      sizeSnapshot(),
+    ],
+  },
+
   {
     input,
     output: { file: pkg.main, format: 'cjs', exports: 'named' },
