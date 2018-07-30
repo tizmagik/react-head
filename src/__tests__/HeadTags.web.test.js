96b21946b81d94ca8e3e51176c05d9e0c0afd96b
zhangciwu <zhangciwu@gmail.com>
2018-07-31 02:27:16 +0800

    Simplify SSR tag removing, also fix #44

--- src/__tests__/HeadTags.web.test.js
+++ src/__tests__/HeadTags.web.test.js
@@ -7,10 +7,12 @@ const ReactDOMMock = {
 };
 jest.setMock('react-dom', ReactDOMMock);
 const removeMock = jest.fn();
-const qsMock = jest.fn(() => ({
-  remove: removeMock,
-}));
-document.head.querySelector = qsMock;
+const qsMock = jest.fn(() => [
+  {
+    remove: removeMock,
+  },
+]);
+document.head.querySelectorAll = qsMock;
 
 describe('HeadTag during client', () => {
   const { HeadProvider, HeadTag, Title, Style, Meta, Link } = require('../');
@@ -30,13 +32,7 @@ describe('HeadTag during client', () => {
   );
 
   it('removes head tags added during ssr', () => {
-    expect(qsMock).toHaveBeenCalledWith(
-      'test[name="x"][content="testing"][data-rh=""]'
-    );
-    expect(qsMock).toHaveBeenCalledWith('title[data-rh=""]');
-    expect(qsMock).toHaveBeenCalledWith('style[data-rh=""]');
-    expect(qsMock).toHaveBeenCalledWith('link[href="index.css"][data-rh=""]');
-    expect(qsMock).toHaveBeenCalledWith('meta[charset="utf-8"][data-rh=""]');
+    expect(qsMock).toHaveBeenCalledWith('[data-rh=""]');
     expect(removeMock).toHaveBeenCalledTimes(5);
   });
 
