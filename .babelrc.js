14c44aa34486dd9543460f9cafd5612fe032dca6
Bogdan Chadkin <trysound@yandex.ru>
2018-08-16 20:42:06 +0300

    Remove prop-types (#56)
    `HeadTag` is implementation detail and aliases always pass tag prop,
    prop-types are not required in this component.
    
    `HeadProvider` prop types are not significant. `headTags` is asserted
    in render. Covering `children` doesn't have any DX matter.

--- .babelrc.js
+++ .babelrc.js
@@ -2,7 +2,6 @@ module.exports = {
   presets: [['@babel/env', { modules: false, loose: true }], '@babel/react'],
   plugins: [
     ['@babel/proposal-class-properties', { loose: true }],
-    ['transform-react-remove-prop-types', { mode: 'unsafe-wrap' }],
     // used for stripping out the `invariant` messages in production builds
     'dev-expression',
   ],
