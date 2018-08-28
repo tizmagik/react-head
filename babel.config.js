b294fa74b4bdc01ae10a23e933da649467eba869
Bogdan Chadkin <trysound@yandex.ru>
2018-08-28 10:22:01 +0300

    Upgrade to babel 7 (#61)

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,8 @@
 module.exports = {
-  presets: [['@babel/env', { modules: false, loose: true }], '@babel/react'],
+  presets: [['@babel/env', { loose: true }], '@babel/react'],
   plugins: [
     ['@babel/proposal-class-properties', { loose: true }],
     // used for stripping out the `invariant` messages in production builds
     'dev-expression',
   ],
-  env: {
-    test: {
-      plugins: ['@babel/transform-modules-commonjs'],
-    },
-  },
 };
