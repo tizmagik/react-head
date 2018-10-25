f1b04fce6624c9275f5b74b4e64d76dee598e197
Umidbek Karimov <uma.karimov@gmail.com>
2018-10-25 12:24:45 +0500

    feat(index.d.ts): Use `React.ComponentType`.

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,37 +1,30 @@
 import {
+  ComponentType,
+  HTMLAttributes,
+  LinkHTMLAttributes,
+  MetaHTMLAttributes,
   ReactElement,
   ReactNode,
-  HTMLAttributes,
   StyleHTMLAttributes,
-  MetaHTMLAttributes,
-  LinkHTMLAttributes,
 } from 'react';
 
 export type TitleProps = HTMLAttributes<HTMLTitleElement>;
-
-export function Title(props: TitleProps): ReactElement<TitleProps>;
+export const Title: ComponentType<TitleProps>;
 
 export type StyleProps = StyleHTMLAttributes<HTMLStyleElement>;
-
-export function Style(props: StyleProps): ReactElement<StyleProps>;
+export const Style: ComponentType<StyleProps>;
 
 export type MetaProps = MetaHTMLAttributes<HTMLMetaElement>;
-
-export function Meta(props: MetaProps): ReactElement<MetaProps>;
+export const Meta: ComponentType<MetaProps>;
 
 export type LinkProps = LinkHTMLAttributes<HTMLLinkElement>;
-
-export function Link(props: LinkProps): ReactElement<LinkProps>;
+export const Link: ComponentType<LinkProps>;
 
 export type HeadTagElement = ReactElement<
   TitleProps | StyleProps | MetaProps | LinkProps
 >;
-
 export interface HeadProviderProps {
   readonly children: ReactNode;
   readonly headTags?: HeadTagElement[];
 }
-
-export function HeadProvider(
-  props: HeadProviderProps
-): ReactElement<HeadProviderProps>;
+export const HeadProvider: ComponentType<HeadProviderProps>;
